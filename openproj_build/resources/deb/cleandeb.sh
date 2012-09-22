18fda6a9e998e1e5fc1a41bbf969fc442c00b844
Laurent chretienneau <laurentc@1504144.no-reply.drupal.org>
2012-09-23 00:25:22 +0200

    buid improvement

---
+++ openproj_build/resources/deb/cleandeb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+P="$1"
+DEB="$2"
+DEB2="$3"
+cd "$P"
+mkdir -p "$P"/tmp
+cd "$P"/tmp
+ar -x "$P"/"$DEB"
+gnutar -xzvf control.tar.gz
+gnutar -xzvf data.tar.gz
+gnutar --numeric-owner --owner=0 --group=0 -czvf control.tar.gz control postinst postrm
+gnutar --numeric-owner --owner=0 --group=0 -czvf data.tar.gz usr
+ar -r "$P"/"$DEB2" debian-binary control.tar.gz data.tar.gz
+cd ..
+rm -rf tmp
