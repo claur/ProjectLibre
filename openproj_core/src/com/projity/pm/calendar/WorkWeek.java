e7c01ceee25186cb519b27b455586c61643af434
claur <claur@users.sourceforge.net>
2015-06-29 16:57:24 +0200

    Import fix for calendars

--- openproj_core/src/com/projity/pm/calendar/WorkWeek.java
+++ openproj_core/src/com/projity/pm/calendar/WorkWeek.java
@@ -30,7 +30,7 @@ in Exhibits A and B of the license at http://www.projity.com/license. You should
 use the latest text at http://www.projity.com/license for your modifications.
 You may not remove this license text from the source files.]
 
-Attribution Information: Attribution Copyright Notice: Copyright © 2006, 2007 
+Attribution Information: Attribution Copyright Notice: Copyright (c) 2006, 2007 
 Projity, Inc. Attribution Phrase (not exceeding 10 words): Powered by OpenProj, 
 an open source solution from Projity. Attribution URL: http://www.projity.com 
 Graphic Image as provided in the Covered Code as file:  openproj_logo.png with 
@@ -138,7 +138,7 @@ public class WorkWeek implements Cloneable,Serializable {
         	if (workDay[i]==null){
         		System.out.println();
         	}
-        		workDay[i].initialize(); // calc hours - fixes bug in importing project 2007 files
+        	else workDay[i].initialize(); // calc hours - fixes bug in importing project 2007 files
         }
         updateWorkingDuration();
     }
--- projectlibre_exchange/src/com/projectlibre/core/pm/exchange/converters/mpx/MpxCalendarConverter.java
+++ projectlibre_exchange/src/com/projectlibre/core/pm/exchange/converters/mpx/MpxCalendarConverter.java
@@ -47,7 +47,7 @@ the CPAL as a work which combines Covered Code or portions thereof with code not
 governed by the terms of the CPAL. However, in addition to the other notice 
 obligations, all copies of the Covered Code in Executable and Source Code form 
 distributed must, as a form of attribution of the original author, include on each 
-user interface screen the "OpenProj"  and “ProjectLibre” logos visible to all users. 
+user interface screen the "OpenProj"  and "ProjectLibre" logos visible to all users. 
 The OpenProj logo should be located horizontally aligned with the menu bar and left 
 justified on the top left of the screen adjacent to the File menu. The logo must be 
 at least 100 x 25 pixels. When users click on the "OpenProj" logo it must direct them 
@@ -68,9 +68,9 @@ the CPAL as a work which combines Covered Code or portions thereof with code not
 governed by the terms of the CPAL. However, in addition to the other notice 
 obligations, all copies of the Covered Code in Executable and Source Code form 
 distributed must, as a form of attribution of the original author, include on each 
-user interface screen the "OpenProj" and “ProjectLibre” logos visible to all users. 
+user interface screen the "OpenProj" and "ProjectLibre" logos visible to all users. 
 The OpenProj logo should be located horizontally aligned with the menu bar and left 
-justified on the top left of the screen adjacent to the File menu.  The logo must be 
+justified on the top left of the screen adjacent to the File menu. The logo must be 
 at least 100 x 25 pixels. When users click on the "OpenProj" logo it must direct them 
 back to http://www.projity.com.
 */
@@ -126,9 +126,13 @@ public class MpxCalendarConverter {
 			WorkDay day=null;
 			if (mpxDay == null) {
 				if (mpxCalendar.isDerived() &&
-							mpxBaseCalendar!=null && 
-							mpxBaseCalendar.isWorkingDay(mpxDayId))
-					day = WorkDay.getNonWorkingDay();				
+							mpxBaseCalendar!=null){
+					if (mpxDayType==net.sf.mpxj.DayType.DEFAULT)
+						day=calendar.getBase().getWeek().getDay(i);
+//						day = WorkDay.getDefaultDay();
+					else if (mpxBaseCalendar.isWorkingDay(mpxDayId)) //TODO correct?
+						day = WorkDay.getNonWorkingDay();
+				}
 			} else {
 				day=new WorkDay(DayType.getInstance(mpxDayType.getValue()));
 				rangeConverter.from(mpxDay,day);
