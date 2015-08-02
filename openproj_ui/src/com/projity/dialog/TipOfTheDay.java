0279efec8f13ba3a92e38a942d81ef42a4bbeecf
claur <claur@users.sourceforge.net>
2015-08-02 16:28:48 +0200

    Russian translation updates

--- openproj_ui/src/com/projity/dialog/TipOfTheDay.java
+++ openproj_ui/src/com/projity/dialog/TipOfTheDay.java
@@ -30,7 +30,7 @@ in Exhibits A and B of the license at http://www.projity.com/license. You should
 use the latest text at http://www.projity.com/license for your modifications.
 You may not remove this license text from the source files.]
 
-Attribution Information: Attribution Copyright Notice: Copyright © 2006, 2007 
+Attribution Information: Attribution Copyright Notice: Copyright ï¿½ 2006, 2007 
 Projity, Inc. Attribution Phrase (not exceeding 10 words): Powered by OpenProj, 
 an open source solution from Projity. Attribution URL: http://www.projity.com 
 Graphic Image as provided in the Covered Code as file:  openproj_logo.png with 
@@ -73,7 +73,13 @@ public class TipOfTheDay {
 			};
 			tipOfTheDayInstance = new JTipOfTheDay(TipLoader.load(Messages.getTipProperties()));
 			int tipNumber=Preferences.userNodeForPackage(TipOfTheDay.class).getInt("tipNumber",1);
-			tipOfTheDayInstance.setCurrentTip(tipNumber);
+			try {
+				tipOfTheDayInstance.setCurrentTip(tipNumber);
+			} catch (Exception e) {
+				tipNumber=1;
+				Preferences.userNodeForPackage(TipOfTheDay.class).putInt("tipNumber",tipNumber);
+				tipOfTheDayInstance.setCurrentTip(tipNumber);
+			}
 		}
 		if (forceShow || tipOfTheDayChoiceInstance.isShowingOnStartup()) {
 			tipOfTheDayInstance.showDialog(owner, tipOfTheDayChoiceInstance,forceShow);
