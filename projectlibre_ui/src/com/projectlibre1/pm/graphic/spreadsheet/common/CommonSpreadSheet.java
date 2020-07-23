132b380a492caa22633d8e82f6606c9ae8c228ae
Dimitri Marques <dimitri.marques@ddembedded.com.br>
2020-07-23 18:10:04 -0300

    enable task vertical scroll

--- projectlibre_ui/src/com/projectlibre1/pm/graphic/spreadsheet/common/CommonSpreadSheet.java
+++ projectlibre_ui/src/com/projectlibre1/pm/graphic/spreadsheet/common/CommonSpreadSheet.java
@@ -505,6 +505,8 @@ public class CommonSpreadSheet extends CommonTable implements CacheListener, Sav
      			JViewport viewport = scrollPane.getViewport();
      			if (viewport == null || viewport.getView() != this) return;
 
+                scrollPane.setAutoscrolls(true);
+                scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
      			configureScrollPaneHeaders(scrollPane);
 
      			Border border = scrollPane.getBorder();
